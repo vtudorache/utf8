dafebcd33f34d63b5213bc70329e0d3bd76fb648
Vlad TUDORACHE <tudorache.vlad@gmail.com>
2020-12-11 23:59:32 +0100

    Updated files.

--- utf8.c
+++ utf8.c
@@ -222,6 +222,7 @@ size_t utf8_to_wchars(wchar_t *buffer, const char *s, size_t count)
 {
     int32_t rune;
     size_t done = 0, parsed, rune_size;
+    wchar_t cache[2];
     if (s == NULL) {
         errno = EINVAL;
         return 0;
@@ -233,11 +234,14 @@ size_t utf8_to_wchars(wchar_t *buffer, const char *s, size_t count)
             errno = EILSEQ;
             return (size_t)-1;
         }
-        rune_size = utf16_encode(buffer, rune);
+        rune_size = utf16_encode(cache, rune);
         if (rune_size > count - done || rune == 0) break;
-        s = &s[parsed];
-        if (buffer != NULL) buffer = &buffer[rune_size];
+        if (buffer != NULL) {
+            *buffer++ = cache[0];
+            if (rune_size > 1) *buffer++ = cache[1];
+        }
         done += rune_size;
+        s += parsed;
     }
     return done;
 }
@@ -246,6 +250,7 @@ size_t utf8_of_wchars(char *buffer, const wchar_t *p, size_t count)
 {
     int32_t rune;
     size_t done = 0, parsed, rune_size;
+    char cache[4];
     if (p == NULL) {
         errno = EINVAL;
         return 0;
@@ -257,24 +262,30 @@ size_t utf8_of_wchars(char *buffer, const wchar_t *p, size_t count)
             errno = EILSEQ;
             return (size_t)-1;
         }
-        rune_size = utf8_encode(buffer, rune);
+        rune_size = utf8_encode(cache, rune);
         if (rune_size > count - done || rune == 0) break;
-        p = &p[parsed];
-        if (buffer != NULL) buffer = &buffer[rune_size];
+        if (buffer != NULL) {
+            *buffer++ = cache[0];
+            if (rune_size > 1) *buffer++ = cache[1];
+            if (rune_size > 2) *buffer++ = cache[2];
+            if (rune_size > 3) *buffer++ = cache[3];
+        }
         done += rune_size;
+        p += parsed;
     }
     return done;
 }
 
 size_t utf8_to_local(char *buffer, const char *s, size_t count)
 {
-    int32_t rune[2] = {0, 0};
+    int32_t rune;
     /*
-    I use an array of two wide chars of which the last is always 0. This 
+    I use an array of three wide chars of which the last is always 0. This 
     allows using the wcstombs function knowing that its source will always 
     end in a 0 rune.
     */
-    wchar_t ws_buffer[2] = {0, 0};
+    wchar_t ws_buffer[] = {0, 0, 0};
+    char mb_buffer[4];
     size_t done = 0, parsed, mb_size;
     if (s == NULL) {
         errno = EINVAL;
@@ -282,18 +293,24 @@ size_t utf8_to_local(char *buffer, const char *s, size_t count)
     }
     if (buffer == NULL) count = (size_t)-1;
     while (done < count) {
-        parsed = utf8_decode(rune, s, (size_t)-1);
+        parsed = utf8_decode(&rune, s, (size_t)-1);
         if (parsed == 0) {
             errno = EILSEQ;
             return (size_t)-1;
         }
-        utf16_encode(ws_buffer, rune[0]); /* only the valid runes get here */
-        mb_size = wcstombs(buffer, ws_buffer, (size_t)-1);
+        ws_buffer[1] = 0;
+        utf16_encode(ws_buffer, rune); /* only the valid runes get here */
+        mb_size = wcstombs(mb_buffer, ws_buffer, (size_t)-1);
         if (mb_size == (size_t)-1) return (size_t)-1; /* can't encode */
         if (mb_size == 0 || mb_size > count - done) break;
-        s = &s[parsed];
-        if (buffer != NULL) buffer = &buffer[mb_size];
+        if (buffer != NULL) {
+            *buffer++ = mb_buffer[0];
+            if (mb_size > 1) *buffer++ = mb_buffer[1];
+            if (mb_size > 2) *buffer++ = mb_buffer[2];
+            if (mb_size > 3) *buffer++ = mb_buffer[3];
+        }
         done += mb_size;
+        s += parsed;
     }
     return done;
 }
@@ -301,7 +318,8 @@ size_t utf8_to_local(char *buffer, const char *s, size_t count)
 size_t utf8_of_local(char *buffer, const char *s, size_t count)
 {
     int32_t rune;
-    wchar_t ws_buffer[2] = {0, 0};
+    wchar_t ws_buffer[] = {0, 0};
+    char cache[4];
     size_t done = 0, mb_size, ws_size, rune_size;
     if (s == NULL) {
         errno = EINVAL;
@@ -312,12 +330,17 @@ size_t utf8_of_local(char *buffer, const char *s, size_t count)
         ws_size = mbstowcs(ws_buffer, s, 1);
         if (ws_size == (size_t)-1) return (size_t)-1;
         utf16_decode(&rune, ws_buffer, (size_t)-1);
-        rune_size = utf8_encode(buffer, rune);
+        rune_size = utf8_encode(cache, rune);
         if (rune_size > count - done || rune == 0) break;
         mb_size = wcstombs(NULL, ws_buffer, (size_t)-1);
-        s = &s[mb_size];
-        if (buffer != NULL) buffer = &buffer[rune_size];
+        if (buffer != NULL) {
+            *buffer++ = cache[0];
+            if (rune_size > 1) *buffer++ = cache[1];
+            if (rune_size > 2) *buffer++ = cache[2];
+            if (rune_size > 3) *buffer++ = cache[3];
+        }
         done += rune_size;
+        s += mb_size;
     }
     return done;
 }
@@ -339,10 +362,9 @@ size_t utf8_to_wchars(wchar_t *buffer, const char *s, size_t count)
             errno = EILSEQ;
             return (size_t)-1;
         }
-        if (buffer != NULL) *buffer = (wchar_t)rune;
+        if (buffer != NULL) *buffer++ = (wchar_t)rune;
         if (rune == 0) break;
-        s = &s[parsed];
-        if (buffer != NULL) buffer = &buffer[1];
+        s += 1;
         done += 1;
     }
     return done;
@@ -363,8 +385,8 @@ size_t utf8_of_wchars(char *buffer, const wchar_t *p, size_t count)
             return (size_t)-1;
         }
         if (rune_size > count - done || *p == 0) break;
-        p = &p[1];
-        if (buffer != NULL) buffer = &buffer[rune_size];
+        if (buffer != NULL) buffer += rune_size;
+        p += 1;
         done += rune_size;
     }
     return done;
@@ -372,8 +394,9 @@ size_t utf8_of_wchars(char *buffer, const wchar_t *p, size_t count)
 
 size_t utf8_to_local(char *buffer, const char *s, size_t count)
 {
-    int32_t rune[2] = {0, 0};
+    int32_t rune[] = {0, 0};
     size_t done = 0, parsed, mb_size;
+    char cache[4];
     if (s == NULL) {
         errno = EINVAL;
         return 0;
@@ -385,19 +408,25 @@ size_t utf8_to_local(char *buffer, const char *s, size_t count)
             errno = EILSEQ;
             return (size_t)-1;
         }
-        mb_size = wcstombs(buffer, rune, (size_t)-1);
+        mb_size = wcstombs(cache, rune, (size_t)-1);
         if (mb_size == (size_t)-1) return (size_t)-1;
         if (mb_size == 0 || mb_size > count - done) break;
-        s = &s[parsed];
-        if (buffer != NULL) buffer = &buffer[mb_size];
+        if (buffer != NULL) {
+            *buffer++ = cache[0];
+            if (mb_size > 1) *buffer++ = cache[1];
+            if (mb_size > 2) *buffer++ = cache[2];
+            if (mb_size > 3) *buffer++ = cache[3];
+        }
         done += mb_size;
+        s += parsed;
     }
     return done;
 }
 
 size_t utf8_of_local(char *buffer, const char *s, size_t count)
 {
-    wchar_t ws_buffer[2] = {0, 0};
+    wchar_t ws_buffer[] = {0, 0};
+    char cache[4];
     size_t done = 0, mb_size, ws_size, rune_size;
     if (s == NULL) {
         errno = EINVAL;
@@ -407,12 +436,17 @@ size_t utf8_of_local(char *buffer, const char *s, size_t count)
     while (done < count) {
         ws_size = mbstowcs(ws_buffer, s, 1);
         if (ws_size == (size_t)-1) return (size_t)-1;
-        rune_size = utf8_encode(buffer, (int32_t)*ws_buffer);
-        if (rune_size > count - done || *ws_buffer == 0) break;
+        rune_size = utf8_encode(cache, (int32_t)*ws_buffer);
+        if (rune_size > count - done || *s == 0) break;
         mb_size = wcstombs(NULL, ws_buffer, (size_t)-1);
-        s = &s[mb_size];
-        if (buffer != NULL) buffer = &buffer[rune_size];
+        if (buffer != NULL) {
+            *buffer++ = cache[0];
+            if (rune_size > 1) *buffer++ = cache[1];
+            if (rune_size > 2) *buffer++ = cache[2];
+            if (rune_size > 3) *buffer++ = cache[3];
+        }
         done += rune_size;
+        s += mb_size;
     }
     return done;
 }
---
+++ utf8_of_local.c
@@ -0,0 +1,66 @@
+#include <locale.h>
+#include <stdio.h>
+
+#include "utf8.h"
+
+int main(int argc, char **argv)
+{
+    int i, j, s_is_alloc;
+    wchar_t *pws;
+    char *r, *s;
+    size_t n;
+    setlocale(LC_ALL, ""); /* this is always necessary */
+    for (i = 1; i < argc; i++) {
+        s_is_alloc = 0;
+        printf("The argument #%d is \"%s\".\n", i, argv[i]);
+        n = utf8_to_wchars(NULL, argv[i], (size_t)-1);
+        if (n == 0) continue;
+        if (n == (size_t)-1) {
+            printf("The string \"%s\" isn't valid UTF-8.\n", argv[i]);
+            printf("Trying to convert from locale...\n");
+            n = utf8_of_local(NULL, argv[i], (size_t)-1);
+            if (n == (size_t)-1) {
+                printf("Can't convert \"%s\" to UTF-8.\n", argv[i]);
+                continue;
+            }
+            s = (char *)malloc((n + 1) * sizeof(char));
+            if (s == NULL) {
+                printf("There is no more memory available.\n");
+                return argc - i;
+            }
+            s_is_alloc = 1;
+            n = utf8_of_local(s, argv[i], n + 1);
+            n = utf8_to_wchars(NULL, s, (size_t)-1);
+        } else {
+            s = argv[i];
+        }
+        printf("It can be converted to %zu non-zero wide characters.\n", n);
+        pws = (wchar_t *)malloc((n + 1) * sizeof(wchar_t));
+        if (pws == NULL) {
+            printf("There is no more memory available.\n");
+            return argc - i;
+        }
+        n = utf8_to_wchars(pws, s, n + 1);
+        printf("The wide character codes are:\n");
+        for (j = 0; j < n; j++) {
+            if (j > 0) putchar(' ');
+            printf("0x%x", pws[j]);
+        }
+        putchar('\n');
+        free(pws);
+        if (s_is_alloc) {
+            n = utf8_to_local(NULL, s, (size_t)-1);
+            if (n > 0) {
+                r = (char *)malloc(n + 1);
+                if (r != NULL) {
+                    n = utf8_to_local(r, s, n + 1);
+                    printf("The string reconverted to locale is \"%s\".\n",
+                        r);
+                    free(r);
+                }
+            }
+            free(s);
+        }
+    }
+    return argc - i;
+}
