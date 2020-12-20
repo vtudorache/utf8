a13d5b49bda2c2a2705471938e0e437292e7ec99
Vlad Tudorache <tudorache.vlad@gmail.com>
2020-12-20 10:51:04 +0100

    Update utf8.c

--- utf8.c
+++ utf8.c
@@ -121,7 +121,7 @@ Gets the next rune in the readable stream `input`.
 Returns the rune.
 Returns 0xfffd if the first characters in stream don't form a valid UTF-8 
 sequence or another error occured.  
-Returns (size_t)-1 if the end-of-file has been reached.
+Returns -1 if the end-of-file has been reached.
 The variable `errno` is set to EILSEQ if an invalid or incomplete sequence 
 was found, or to the last error code set by the standard library function 
 `fgetc`.
@@ -189,7 +189,7 @@ size_t utf8_encode(char *p, int32_t rune)
 /*
 Puts in the writable stream `output` the UTF-8 bytes encoding `rune`.
 Returns the value of `rune` in the absence of error.
-Returns (size_t)-1 if the operation fails. The `errno` variable is set to
+Returns -1 if the operation fails. The `errno` variable is set to
 EILSEQ if `rune` isn't a valid code point or to the last error code set by
 the standard library function `fputc`.
 */
