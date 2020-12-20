fee4e565c66168db4825f67cf5b5f56238dce1db
Vlad Tudorache <tudorache.vlad@gmail.com>
2020-12-20 10:51:43 +0100

    Update utf8.h

--- utf8.h
+++ utf8.h
@@ -11,7 +11,7 @@
 Returns the rune.
 Returns 0xfffd if the first characters in stream don't form a valid UTF-8 
 sequence or another error occured.  
-Returns (size_t)-1 if the end-of-file has been reached.
+Returns -1 if the end-of-file has been reached.
 The variable `errno` is set to EILSEQ if an invalid or incomplete sequence 
 was found, or to the last error code set by the standard library function 
 `fgetc`.
@@ -22,7 +22,7 @@ int32_t utf8_get_rune(FILE *input);
 `utf8_put_rune` puts in the writable stream `output` the UTF-8 bytes 
 encoding `rune`.
 Returns the value of `rune` in the absence of error.
-Returns (size_t)-1 if the operation fails. The `errno` variable is set to
+Returns -1 if the operation fails. The `errno` variable is set to
 EILSEQ if `rune` isn't a valid code point or to the last error code set by
 the standard library function `fputc`.
 */
